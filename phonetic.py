a3ead65f6552b9ce506e67a39e925e7729a67a37
1111211047 <151598128+1111211047@users.noreply.github.com>
2024-06-24 17:15:19 +0800

    Update phonetic.py

--- phonetic.py
+++ phonetic.py
@@ -5,12 +5,3 @@ def read(expression):
         return(f"{expression} = {result}")
     except Exception as e:
         return(f"無法计算 '{expression}': {e}")
-
-if __name__ == "__main__":
-    while True:
-        expression = input("请输入乘法表达式 (例如 1*1)，或輸入 'exit' 退出程序: ")
-        
-        if expression.lower() == 'exit':
-            print("程序已退出。")
-            break
-        
